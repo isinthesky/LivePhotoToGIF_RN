20ac59ba385ec29d63a31813e684f0d9f590757c
isinthesky <hijde@naver.com>
2023-04-25 00:40:35 +0900

    feat: option send, gif view

--- src/components/OptionScroll.js
+++ src/components/OptionScroll.js
@@ -1,38 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-
-import { View, Text, Image, StyleSheet } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { PickerIOS } from "@react-native-picker/picker";
+import { updateValue } from "../features/reducers/optionSlice";
+import { SIGNATURE_COLOR } from "../constants/color";
 
-function OptionScroll({
-  selectOptions,
-  titleText,
-  titleImage,
-  typeText,
-  onValue,
-  value,
-}) {
+function OptionScroll({ selectOptions, titleText, titleImage, typeText }) {
+  const dispatch = useDispatch();
+  const [pickerOn, setPickerOn] = useState(false);
+
+  const value = useSelector((state) => state.optionReducer.value[titleText]);
+
+  const update = (e) => {
+    dispatch(updateValue({ option: titleText, value: e }));
+  };
+
+  const togglePicker = () => {
+    setPickerOn(!pickerOn);
+  };
+
   return (
-    <View style={styles.gifOption}>
-      <Text style={styles.optionText}>{titleText}</Text>
-      <Image source={titleImage} style={styles.optionImage} />
-      <PickerIOS
-        selectedValue={value}
-        itemStyle={styles.picker}
-        style={styles.picker}
-        onValueChange={onValue}
-      >
-        {selectOptions.map((item) => {
-          return (
-            <PickerIOS.Item
-              label={item.toString()}
-              key={item.toString()}
-              value={item.toString()}
-            />
-          );
-        })}
-      </PickerIOS>
-      <Text style={styles.optionValue}>{typeText}</Text>
+    <View style={pickerOn ? styles.OptionContainer2 : styles.OptionContainer}>
+      <View style={styles.gifOption}>
+        <Text style={styles.optionText}>{titleText}</Text>
+        <Image source={titleImage} style={styles.optionImage} />
+        <TouchableOpacity onPress={togglePicker}>
+          <Text style={styles.optionValue}>{value}</Text>
+        </TouchableOpacity>
+        <Text style={styles.optionType}>{typeText}</Text>
+      </View>
+
+      {pickerOn && (
+        <PickerIOS
+          selectedValue={value}
+          itemStyle={styles.pickerText}
+          style={styles.picker}
+          onValueChange={update}
+        >
+          {selectOptions.map((item) => {
+            return (
+              <PickerIOS.Item
+                label={item.toString()}
+                key={item.toString()}
+                value={item.toString()}
+              />
+            );
+          })}
+        </PickerIOS>
+      )}
     </View>
   );
 }
@@ -40,28 +56,68 @@ function OptionScroll({
 OptionScroll.propTypes = {
   selectOptions: PropTypes.array.isRequired,
   titleText: PropTypes.string.isRequired,
-  titleImage: PropTypes.string.isRequired,
+  titleImage: PropTypes.any.isRequired,
   typeText: PropTypes.string.isRequired,
-  onValue: PropTypes.object.isRequired,
-  value: PropTypes.number.isRequired,
 };
 
 const styles = StyleSheet.create({
+  OptionContainer: {
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+    paddingTop: 15,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 15,
+    width: "100%",
+  },
+  OptionContainer2: {
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+    paddingTop: 15,
+    paddingLeft: 10,
+    paddingRight: 10,
+    width: "100%",
+  },
   gifOption: {
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
-    padding: 10,
     width: "100%",
   },
-  optionText: { fontSize: 24, width: 150, textAlign: "center" },
-  optionImage: { width: 30, height: 30 },
+  optionText: { fontSize: 24, width: 130, textAlign: "center" },
   optionValue: {
+    fontSize: 24,
+    width: 100,
+    textAlign: "center",
+    fontWeight: "bold",
+    color: SIGNATURE_COLOR,
+  },
+  optionImage: { width: 30, height: 30 },
+  optionType: {
     fontSize: 20,
     width: 40,
     textAlign: "left",
   },
-  picker: { width: 100, height: 40 },
+  picker: {
+    left: 33,
+    width: 100,
+    height: 80,
+    fontSize: 20,
+    lineHeight: 19,
+    textAlign: "center",
+  },
+  pickerText: {
+    left: 33,
+    width: 100,
+    height: 100,
+    fontSize: 20,
+    lineHeight: 19,
+    textAlign: "center",
+    color: SIGNATURE_COLOR,
+    fontWeight: "bold",
+  },
 });
 
 export default OptionScroll;
